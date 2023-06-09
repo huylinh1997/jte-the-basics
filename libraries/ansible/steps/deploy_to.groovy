d346a297b898915bab7698722a9502127bbe2258
dinhhuy <huyhuong19972015@gmail.com>
2023-06-09 10:25:45 +0700

    add global config

--- libraries/ansible/steps/deploy_to.groovy
+++ libraries/ansible/steps/deploy_to.groovy
@@ -1,5 +1,7 @@
 void call(environment){
     stage("Deploy To: ${environment.environment.APP_NAME}"){
         println "performing a deployment through ansible.."
+        println "Lionel ${env.API_PACKAGE_NAME}"
+
     }
 }
